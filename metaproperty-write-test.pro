bd522c82c4dad1ec5c041d8da3eaa85b4bdf8313
Christian Ehringfeld <c.ehringfeld@t-online.de>
2016-05-07 01:28:27 +0200

    update

---
+++ metaproperty-write-test.pro
@@ -0,0 +1,22 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-05-07T01:23:38
+#
+#-------------------------------------------------
+
+QT       += testlib
+
+QT       -= gui
+
+TARGET = tst_metapropertywritetest
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += tst_metapropertywritetest.cpp
+DEFINES += SRCDIR=\\\"$$PWD/\\\"
+
+HEADERS += \
+    models.h
---
+++ tst_metapropertywritetest.cpp
@@ -0,0 +1,46 @@
+#include <QString>
+#include <QtTest>
+#include <QMetaObject>
+#include <QVariant>
+#include <QMetaProperty>
+#include <QDebug>
+#include <QObject>
+#include <QList>
+#include <QSharedPointer>
+#include "models.h"
+
+class MetapropertyWriteTest : public QObject {
+    Q_OBJECT
+
+  public:
+    MetapropertyWriteTest();
+    void setListProperty(const QSharedPointer<Entity> &entity,
+                         QList<QSharedPointer<Entity>> &list, const QMetaProperty &property);
+
+  private Q_SLOTS:
+    void testCase1();
+};
+
+MetapropertyWriteTest::MetapropertyWriteTest() {
+}
+
+void MetapropertyWriteTest::testCase1() {
+    QSharedPointer<Entity> p = QSharedPointer<Entity>(new Person());
+    const QMetaObject *mo = p->metaObject();
+    QMetaProperty prop = mo->property(1);
+    QList<QSharedPointer<Entity>> list;
+    list.append(p);
+    setListProperty(p, list, prop);
+}
+
+
+void MetapropertyWriteTest::setListProperty(const QSharedPointer<Entity> &entity,
+        QList<QSharedPointer<Entity>> &list, const QMetaProperty &property)  {
+    QVariant var;
+    var.setValue<QList<QSharedPointer<Entity>>>(list);
+    QVERIFY(property.write(entity.data(), var));
+}
+
+QTEST_APPLESS_MAIN(MetapropertyWriteTest)
+
+#include "tst_metapropertywritetest.moc"
